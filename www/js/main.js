4943fd3179545f2077552a71531876c0628b9995
Wesley Flôres <wesleyfloresterres@gmail.com>
2021-01-14 11:44:09 -0300

    main.js

--- www/js/main.js
+++ www/js/main.js
@@ -1,3 +1,65 @@
+class MediaTrack {
+    /**
+     * Back.
+     * 
+     * @var string
+     */
+    static ENVIRONMENT = "environment";
+
+    /**
+     * Frontal.
+     * 
+     * @var string
+     */
+    static USER = "user";
+
+    /**
+     * Constructor.
+     * 
+     * @param {Void}
+     */
+    constructor() { }
+
+    /**
+     * Init.
+     * 
+     * @param {Void}
+     */
+    init() { }
+
+    /**
+     * Post.
+     * 
+     * @param {String} base64
+     */
+    post(ImageBase64) {
+        const HTTPRequest = new XMLHttpRequest();
+
+        HTTPRequest.open("POST", "save_photos.php", true);
+        HTTPRequest.setRequestHeader("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
+
+        HTTPRequest.onload = function () {
+            if (HTTPRequest.status >= 200 && HTTPRequest.status < 400) {
+                const data = JSON.parse(HTTPRequest.responseText);
+
+                if (data.error) {
+                    alert(data.error);
+
+                    return false;
+                }
+            } else {
+                alert("Erro ao salvar. Tipo:" + HTTPRequest.status);
+            }
+        };
+
+        HTTPRequest.onerror = function () {
+            alert("Erro ao salvar. Back-End inacessível.");
+        }
+
+        HTTPRequest.send(`path=${ImageBase64}`);
+    }
+}
+
 /**
  * 
  */
@@ -9,7 +71,7 @@ function loadCamera() {
     video.setAttribute("playsinline", "");
 
     if (navigator.mediaDevices.getUserMedia) {
-        navigator.mediaDevices.getUserMedia({ audio: false, video: { facingMode: "user" } })
+        navigator.mediaDevices.getUserMedia({ audio: false, video: { facingMode: MediaTrack.USER } })
             .then(function (stream) {
                 video.srcObject = stream;
             })
@@ -34,36 +96,5 @@ function take() {
 
     const base64 = canvas.toDataURL("image/jpeg");
 
-    store(base64);
-}
-
-/**
- * 
- * @param {String} base64 
- */
-function store(base64) {
-    const HTTPRequest = new XMLHttpRequest();
-
-    HTTPRequest.open("POST", "save_photos.php", true);
-    HTTPRequest.setRequestHeader("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
-
-    HTTPRequest.onload = function () {
-        if (HTTPRequest.status >= 200 && HTTPRequest.status < 400) {
-            const data = JSON.parse(HTTPRequest.responseText);
-
-            if (data.error) {
-                alert(data.error);
-
-                return false;
-            }
-        } else {
-            alert("Erro ao salvar. Tipo:" + HTTPRequest.status);
-        }
-    };
-
-    HTTPRequest.onerror = function () {
-        alert("Erro ao salvar. Back-End inacessível.");
-    }
-
-    HTTPRequest.send(`path=${base64}`);
+    (new MediaTrack()).post(base64);
 }
